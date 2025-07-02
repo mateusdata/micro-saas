d6706585ef6e2d733387565ae5aee3f372989def
Mateus Santos <mateuspele2015@gmail.com>
2025-07-02 02:22:59 -0300

    feat: added suport for docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm ci --only=production
+COPY . .
+RUN npm run build
+
+EXPOSE 3000
+ENV NODE_ENV=production
+ENV PORT=3000
+
+# Comando para iniciar a aplicação
+CMD ["npm", "start"]
---
+++ app/api/login/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export  async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { email, password } = body;
+  return NextResponse.json({ message: { email, password } }, { status: 200 });
+}
